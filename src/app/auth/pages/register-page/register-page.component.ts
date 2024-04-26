55326e884bae4203b2ba44c418231eb3de6e566b
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-26 23:59:32 +0200

    Register

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
-  templateUrl: './register-page.component.html',
-  styles: ``
+	templateUrl: './register-page.component.html',
+	styles: ``,
 })
 export class RegisterPageComponent {
+	private fb = inject(FormBuilder);
+	private authService = inject(AuthService);
+	private router = inject(Router);
+	public myForm: FormGroup = this.fb.group({
+		name: ['', [Validators.required]],
+		email: ['', [Validators.required, Validators.email]],
+		password: ['', [Validators.required, Validators.minLength(6)]],
+	});
 
+	register() {
+		const { name, email, password } = this.myForm.value;
+		this.authService.register(name, email, password).subscribe({
+			next: () => this.router.navigateByUrl('/dashboard'),
+			error: (message) => {
+				Swal.fire('Error', message, 'error');
+			},
+		});
+	}
 }
