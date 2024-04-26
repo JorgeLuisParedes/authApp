bad1711478f1b6992d2faab8247b4f8333239b41
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-26 02:51:07 +0200

    Post - Login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environments';
-import { Observable, of } from 'rxjs';
-import { AuthStatus, User } from '../interfaces';
+import { Observable, map, of, tap } from 'rxjs';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,12 +10,28 @@ import { AuthStatus, User } from '../interfaces';
 export class AuthService {
 	private readonly baseUrl: string = environment.baseUrl;
 	private http = inject(HttpClient);
-	private_currentUser = signal<User | null>(null);
+	private _currentUser = signal<User | null>(null);
 	private _authStatus = signal<AuthStatus>(AuthStatus.cheking);
+
+	public currentUser = computed(() => this._currentUser());
+	public authStatus = computed(() => this._authStatus());
 
 	constructor() {}
 
 	login(email: string, password: string): Observable<boolean> {
-		return of(true);
+		const url = `${this.baseUrl}/auth/login`;
+		const body = { email, password };
+
+		return this.http.post<LoginResponse>(url, body).pipe(
+			tap(({ user, token }) => {
+				this._currentUser.set(user);
+				this._authStatus.set(AuthStatus.authenticated);
+				localStorage.setItem('token', token);
+				console.log({ user, token });
+			}),
+			map(() => true)
+
+			// TODO: errores
+		);
 	}
 }
